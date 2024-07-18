36a9ad6b897d719fcfc19393bf09fea06bdbc809
Josh Gumerove <jgumerove@kaufmanrossin.com>
2024-07-18 09:14:28 -0400

    display choices

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Hello!");
+Console.WriteLine("What do you want to do?");
+DisplayUserChoices();
+
+
+
+Console.ReadKey();
+void DisplayUserChoices()
+{
+    Console.WriteLine("[S]ee all TODOs");
+    Console.WriteLine("[A]dd a TODO");
+    Console.WriteLine("[R]emove a TODO");
+    Console.WriteLine("[E]xit");
+}
